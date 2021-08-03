fb26e4d807df4af307d1eea4e1630891de49a9de
Joshua Pregbaha <jpregbaha@Suzanne.local>
2021-08-03 17:59:47 +0100

    Decision Making

--- decision_making.sh
+++ decision_making.sh
@@ -4,4 +4,25 @@
 # This script shows how decision making works
 # Script follows here:
 
+echo -n "Enter the name of a country: "
+read COUNTRY
 
+echo -n "The official language of $COUNTRY is "
+
+case $COUNTRY in
+	Lithuania)
+		echo -n "Lithuanian"
+		;;
+
+	Romania | Moldova)
+		echo -n "Romanian"
+		;;
+
+	Italy | "San Marino" | Switzerland | "Vatican City")
+		echo -n "Italian"
+		;;
+
+	*)
+		echo -n "...I don't know buddy"
+		;;
+esac
